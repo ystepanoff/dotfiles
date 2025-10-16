108b30117cd72f6e053a6411a2b09c3af515cbca
Yegor Stepanov <ystepanoff@icloud.com>
2025-10-16 19:02:40 +0100

    fix up nvimtree mappings

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -1,3 +1,11 @@
+local function set_mappings(bufnr)
+  local api = require('nvim-tree.api')
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', '-', api.tree.change_root_to_parent, { buffer = bufnr, noremap = true, silent = true })
+  vim.keymap.set('n', 'u', api.tree.change_root_to_parent, { buffer = bufnr, noremap = true, silent = true })
+  vim.keymap.set('n', '<CR>', api.node.open.edit, { buffer = bufnr, noremap = true, silent = true })
+end
+
 return {
   { 'nvim-tree/nvim-web-devicons', lazy = true },
   {
@@ -5,28 +13,26 @@ return {
     version = '*',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
-      { '<leader>e', function() require('nvim-tree.api').tree.toggle({ find_file = true, focus = true }) end,
-        desc = 'Explorer (nvim-tree)' },
-      { '<leader>E', function() require('nvim-tree.api').tree.find_file({ open = true, focus = true }) end,
-        desc = 'Reveal file in explorer' },
+      { '<leader>e', function() require('nvim-tree.api').tree.toggle({ find_file = true, focus = true }) end, desc = 'Explorer (nvim-tree)' },
+      { '<leader>E', function() require('nvim-tree.api').tree.find_file({ open = true, focus = true }) end, desc = 'Reveal file in explorer' },
     },
     opts = {
+      on_attach = function(bufnr)
+        set_mappings(bufnr)
+      end,
       disable_netrw = true,
       hijack_netrw = true,
-
       sync_root_with_cwd = true,
       respect_buf_cwd = true,
       update_focused_file = {
         enable = true,
-        update_root = false,
+        update_root = true,
       },
-
       view = {
         width = 34,
         side = 'left',
         preserve_window_proportions = true,
       },
-
       renderer = {
         highlight_git = true,
         highlight_opened_files = 'name',
@@ -37,12 +43,10 @@ return {
           show = { folder_arrow = true, file = true, folder = true, git = true },
         },
       },
-
       filters = {
         dotfiles = false,
         git_ignored = false,
       },
-
       git = { enable = true, ignore = false, timeout = 400 },
       diagnostics = {
         enable = true,
@@ -50,7 +54,6 @@ return {
         show_on_open_dirs = false,
         debounce_delay = 100,
       },
-
       actions = {
         open_file = {
           quit_on_open = false,
@@ -62,25 +65,17 @@ return {
         },
         change_dir = { enable = true, restrict_above_cwd = false },
       },
-
       live_filter = { prefix = ' ', always_show_folders = false },
-
-      -- Trash vs permanent delete (adjust to taste)
-      -- NOTE: nvim-tree uses vim.fn.delete by default (permanent).
-      -- To move to trash on macOS, set this and have `trash` cli installed:
-      -- system_open = { cmd = 'open' }, -- for opening files externally
     },
     config = function(_, opts)
       require('nvim-tree').setup(opts)
-
-      local api = require('nvim-tree.api')
       vim.api.nvim_create_autocmd('QuitPre', {
         callback = function()
           local wins = vim.api.nvim_list_wins()
           if #wins == 1 then
             local bufname = vim.api.nvim_buf_get_name(0)
             if bufname:match('NvimTree_') then
-              api.tree.close()
+              require('nvim-tree.api').tree.close()
             end
           end
         end,
