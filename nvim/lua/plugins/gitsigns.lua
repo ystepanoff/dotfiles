a23c60e65b5e3ba31e4473205d51341e91876428
Yegor Stepanov <ystepanoff@icloud.com>
2026-02-06 17:40:39 +0000

    keymap bindings for gitlens

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,30 @@
 return {
-  { 'lewis6991/gitsigns.nvim', opts = {} },
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      on_attach = function(bufnr)
+        local gs = require('gitsigns')
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+        end
+
+        map('n', ']h', gs.next_hunk, 'Next git hunk')
+        map('n', '[h', gs.prev_hunk, 'Previous git hunk')
+
+        map('n', '<leader>hp', gs.preview_hunk, 'Preview git hunk')
+        map('n', '<leader>hi', gs.preview_hunk_inline, 'Preview hunk inline')
+
+        map('n', '<leader>hs', gs.stage_hunk, 'Stage hunk')
+        map('n', '<leader>hr', gs.reset_hunk, 'Reset hunk')
+        map('n', '<leader>hS', gs.stage_buffer, 'Stage buffer')
+        map('n', '<leader>hu', gs.undo_stage_hunk, 'Undo stage hunk')
+        map('n', '<leader>hR', gs.reset_buffer, 'Reset buffer')
+        map('n', '<leader>hb', function() gs.blame_line({ full = true }) end, 'Blame line')
+        map('n', '<leader>hd', gs.diffthis, 'Diff against index')
+        map('n', '<leader>hD', function() gs.diffthis('~') end, 'Diff against last commit')
+        map('n', '<leader>tb', gs.toggle_current_line_blame, 'Toggle line blame')
+        map('n', '<leader>td', gs.toggle_deleted, 'Toggle show deleted')
+      end,
+    },
+  },
 }
